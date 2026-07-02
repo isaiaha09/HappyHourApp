bae45115eb9c375a975606fab1619542d6124567
isaiaha09 <isaiah.aris@gmail.com>
2026-07-01 17:05:32 -0700

    one last time please

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1469,7 +1469,7 @@ function AppScreen() {
       },
     ],
   };
-  const loginSuccessBottomNavLift = Math.max(0, insets.bottom - 2);
+  const loginSuccessBottomNavLift = Math.max(0, insets.bottom - 1);
   const loginSuccessBottomNavStyle = {
     transform: [
       {
